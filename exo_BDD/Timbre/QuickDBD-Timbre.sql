e87877beb8a81538a40d8b14d216747e51029804
Safia54 <sbihmedn@gmail.com>
2017-08-17 11:10:26 +0200

    mon premier commit

---
+++ exo_BDD/Timbre/QuickDBD-Timbre.sql
@@ -0,0 +1,31 @@
+﻿-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/schema/8RQWI5bLAEOtUKtCi2ctSQ
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+CREATE TABLE "Timbres" (
+    "ID" int  NOT NULL ,
+    "Name1" string  NOT NULL ,
+    "Name2" string  NOT NULL ,
+    "Name3" string  NOT NULL ,
+    "Name4" string  NOT NULL ,
+    "Name5" string  NOT NULL ,
+    CONSTRAINT "pk_Timbres" PRIMARY KEY (
+        "ID"
+    )
+)
+
+GO
+
+CREATE TABLE "Collection" (
+    "Id" int  NOT NULL ,
+    "Belgique" string  NOT NULL ,
+    "France" string  NOT NULL ,
+    "Canada" string  NOT NULL ,
+    "Maroc" string  NOT NULL ,
+    CONSTRAINT "pk_Collection" PRIMARY KEY (
+        "Id"
+    )
+)
+
+GO
+
